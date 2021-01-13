f1aa05a242c848926b719fd77d045faa83fed7b5
Wiggie-Scripts <77380960+Wiggie-Scripts@users.noreply.github.com>
2021-01-13 09:09:55 +0100

    Add files via upload

---
+++ wiggie_clearchat/client.lua
@@ -0,0 +1,6 @@
+--###############################################--
+--#      Made by Wiggie#0001                    #--
+--###############################################--
+RegisterCommand('clear', function(source, args)
+    TriggerEvent('chat:clear')
+end, false)
